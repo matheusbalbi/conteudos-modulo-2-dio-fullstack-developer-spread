d2526b0c9dd022b73fc5e7929efa968c96cf836a
matheusbalbi <balbioficial@gmail.com>
2022-06-22 23:34:42 -0300

    arquivos modulo 2 fullstack developer spread dio.

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+	<head>
+		<title>Meu primeiro site em PHP! Woohoo!</title>
+	</head>
+	
+	<body>
+	
+		<?php		//coloca para abrir código php
+			for ($i = 0 ; $i < 10 ; $i++) {
+				print ( "Linha número " . $i . "<br/>" );
+			}
+		?>
+		
+		</body>
+		
+		</html>
